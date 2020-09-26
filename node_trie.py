52fcb3bd3daa02b6349c5c9095f18d2acfb5f3bc
Pietro Malky <pietromalky@gmail.com>
2020-09-25 23:12:39 -0500

    search works

--- node_trie.py
+++ node_trie.py
@@ -71,13 +71,30 @@ class Trie:
                 i=0
             )
 
-    def _search_word(self, word: str) -> bool:
+    def _search_word(self, word: str, w: int, children: list) -> bool:
+        if w >= len(word):
+            return True
+
+        i: int = 0
+        while i < len(children):
+            if word[w] == children[i].value:
+                return self._search_word(
+                    word=word,
+                    w=w+1,
+                    children=children[i].children
+                )
+            i += 1
+
         return False
 
     # public methods
 
-    def search_word(self, word: str):
-        return self._search_word(word=word)
+    def search_word(self, word: str) -> bool:
+        return self._search_word(
+            word=word,
+            w=0,
+            children=self.children
+        )
 
     def add_word(self, word: str):
         self._add_word(word=word, w=0, children=self.children, i=0)
@@ -98,6 +115,6 @@ with open("./data.txt", "r") as fin:
 t.print()
 
 
-word = "ace"
+word = "contr"
 word_found = t.search_word(word=word)
 print("was %s found? %s" % (word, word_found))
