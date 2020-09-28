9ee9273e39f47986adf8ff2217426b48eb9952fe
Pietro Malky <pietromalky@gmail.com>
2020-09-28 11:53:23 -0500

    added the oddessey text

--- main.py
+++ main.py
@@ -36,14 +36,13 @@ def trie_based_search():
 
     t.save_trie_to_disk()
 
-    while True:
-        print("Number of unique beginning chars: %s" % len(t.children))
-        word = input("Enter a word to search in %s: " % text_path)
-        search_start = time()
-        word_found = t.search_word(word=word)
-        search_end = time()
-        print("\twas %s found? %s\n" % (word, word_found))
-        print("\ttime for search: %d" % (search_end - search_start))
+    print("Number of unique beginning chars: %s" % len(t.children))
+    word = input("Enter a word to search in %s: " % text_path)
+    search_start = time()
+    word_found = t.search_word(word=word)
+    search_end = time()
+    print("\twas %s found? %s\n" % (word, word_found))
+    print("\ttime for search: %d" % (search_end - search_start))
 
 
 def brute_force_search():
@@ -53,39 +52,38 @@ def brute_force_search():
 
         text = fin.readlines()
 
-        while True:
-            target = input("Enter a word to search in %s: " % text_path)
-            target_found = False
+        target = input("Enter a word to search in %s: " % text_path)
+        target_found = False
 
-            line: str
-            search_start = time()
+        line: str
+        search_start = time()
 
-            for i, line in enumerate(text):
-                # replace all non-alphanumeric characters in string with empty str
-                # generate list of words in line by splitting by space
-                words = (re.sub(r"[^a-zA-Z0-9_ ]+", "", line)).split(' ')
-                # iterate through each word in list
-                for word in words:
-                    break_loop = False
-                    if len(word) == len(target):
-                        if word[0] == target[0]:
-                            t = 0
-                            while target[t] == word[t]:
-                                t += 1
-                                if t == len(target) - 1:
-                                    target_found = True
-                                    break_loop = True
-                                    break
-                    if break_loop:
-                        break
-            search_end = time()
+        for i, line in enumerate(text):
+            # replace all non-alphanumeric characters in string with empty str
+            # generate list of words in line by splitting by space
+            words = (re.sub(r"[^a-zA-Z0-9_ ]+", "", line)).split(' ')
+            # iterate through each word in list
+            for word in words:
+                break_loop = False
+                if len(word) == len(target):
+                    if word[0] == target[0]:
+                        t = 0
+                        while target[t] == word[t]:
+                            t += 1
+                            if t == len(target) - 1:
+                                target_found = True
+                                break_loop = True
+                                break
+                if break_loop:
+                    break
+        search_end = time()
 
-            print("time for search: %s\n\n" % (
-                search_end - search_start
-            ))
+        print("time for search: %s\n\n" % (
+            search_end - search_start
+        ))
 
-            print("\twas %s found? %s\n" % (target, target_found))
+        print("\twas %s found? %s\n" % (target, target_found))
 
 
 if __name__ == "__main__":
-    text_path = "./data/shakespeare.txt"
+    text_path = "./data/oddessy.txt"
